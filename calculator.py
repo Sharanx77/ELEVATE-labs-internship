825a3507b8bf9326723a36c89622949cb78f814f
B Sharana Basava <b.sharanabasava2006@gmail.com>
2025-08-05 07:59:10 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("A simple calculator")
+
+
+while True:
+    num1=float(input("Enter first number: "))
+    num2=float(input("Enter second number: "))
+
+    op=input("Enter operation (+, -, *, /): ")
+    match op:
+        case '+':
+            result = num1 + num2
+        case '-':
+            result = num1 - num2   
+        case '*':
+            result = num1 * num2    
+        case '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Error! Division by zero."
+    print(f"The result is: {result}")
+    print("Do you want to perform another calculation? (yes/no)")
+    cont = input().lower()
+    if cont != 'yes':
+        break
